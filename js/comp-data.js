6bb1abe6bec09ad146bb0fb6f15a637a24caf6a6
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-04-30 22:36:45 -0400

    Added in some groundwork for the sys-info module

--- js/comp-data.js
+++ js/comp-data.js
@@ -1,10 +1,14 @@
 const socket = io.connect('http://localhost');
 let cpuSpd = document.getElementById("processor-speed");
+let gpu = document.getElementById("gpu");
+let resolution = document.getElementById("sys-screen");
 
 socket.on('CPU_METRICS', (data) => {
   cpuSpd.innerHTML = Math.round(data.load) + "%";
 })
 
+
+//Normally I wouldn't use a socket here... but it's convenient to all have in one place
 socket.on('HARDWARE_INFO', (data) => {
   //Write hardware info to the screen
 })
@@ -16,5 +20,7 @@ socket.on('NETWORK_INFO', (data) => {
 
 socket.on('DISPLAY_INFO', (data) => {
   console.log(data);
+  gpu.innerHTML = data.gpu;
+  resolution.innerHTML = data.resolution.x + "x" + data.resolution.y;
   //write to screen
 })

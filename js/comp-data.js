f5ab948f43594175ffa549d7dd349811dfe23d14
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-05-01 17:41:12 -0400

    Adding in features for future sys-info additions

--- js/comp-data.js
+++ js/comp-data.js
@@ -3,19 +3,17 @@ let cpuSpd = document.getElementById("processor-speed");
 let gpu = document.getElementById("gpu");
 let resolution = document.getElementById("sys-screen");
 
+//Chart
+
+//Sockets
 socket.on('CPU_METRICS', (data) => {
   cpuSpd.innerHTML = Math.round(data.load) + "%";
 })
 
-
 //Normally I wouldn't use a socket here... but it's convenient to all have in one place
 socket.on('HARDWARE_INFO', (data) => {
+  console.log(data);
   //Write hardware info to the screen
-})
-
-socket.on('NETWORK_INFO', (data) => {
-  //Write network info to the screen
-  //IPv4
 })
 
 socket.on('DISPLAY_INFO', (data) => {
@@ -24,3 +22,7 @@ socket.on('DISPLAY_INFO', (data) => {
   resolution.innerHTML = data.resolution.x + "x" + data.resolution.y;
   //write to screen
 })
+
+socket.on('MEM_INFO', (data) => {
+  console.log(data);
+});
--- js/node/sys-info.js
+++ js/node/sys-info.js
@@ -5,10 +5,12 @@ let io = require('socket.io')(expServer);
 let currentLoad;
 let resolution;
 let gpu;
+let cpuInfo;
+let mbInfo;
+let memInfo;
 
 io.on('connection', function (socket) {
   console.log("Connection Successful");
-
 
   //Sending CPU processing speed
   setInterval( () => {
@@ -21,6 +23,26 @@ io.on('connection', function (socket) {
     }, 1000
   );
 
+  //Websockets are overkill, but it was convenient to keep it all in one place like this
+  si.cpu( (data) => {
+    cpuInfo = {
+      manufacturer: data.manufacturer,
+      brand: data.brand,
+      speed: data.speed + "GHz",
+      cores: data.cores + " core"
+    }
+    si.baseboard( (data) => {
+      mbInfo = {
+        maker: data.manufacturer,
+        model: data.model
+      }
+        //This is some ugly nesting. But... It works I guess?
+      socket.emit('HARDWARE_INFO', {
+        cpu: cpuInfo,
+        mb: mbInfo,
+      })
+    })
+  })
 
   //Sending GPU and resolution information
   si.graphics( (data) => {
@@ -36,4 +58,18 @@ io.on('connection', function (socket) {
     })
   });
 
+//TODO - Fix this. I'm querying the RAM not the Harddrives like I want
+  si.mem( (data) => {
+    memInfo = {
+      total: data.total,
+      used: data.used,
+      available: data.available
+    }
+    console.log(memInfo);
+
+    socket.emit('MEM_INFO', {
+      memInfo: memInfo,
+    })
+
+  });
 });

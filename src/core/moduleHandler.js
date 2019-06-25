cc76ef08b04ea03a5103434dcd4776474689da77
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-06-25 13:28:56 -0400

    Added in ability to reference external stylesheets

--- src/core/moduleHandler.js
+++ src/core/moduleHandler.js
@@ -1,7 +1,10 @@
 let config = require('../config/config.js');
 let modDict = {}; //Storing all of our modules by name
 let currentMod; //Reference variable for new modules
-let cssPile = [];
+let cssPile = {
+  local: [],
+  external: []
+};
 let DOMPile = { // TODO - add in dynamic columns to config file and instantiate here
   0: ``,
   1: ``,
@@ -18,16 +21,15 @@ for(let i=0; i<config.modules.length; i++){
   currentMod = require(`../modules/${curName}/${curName}.js`);
   currentMod = new currentMod(config.modules[i]);
   //Adding HTML to the template string
-  console.log(config.modules[i].location);
   DOMPile[config.modules[i].location] += currentMod.getDOM;
   //Adding CSS references
   // TODO - Think about making this specific using IDs or classes --> Take some work off of the user
   currentMod.setCSS = currentMod.defineCSS();
-  cssPile.push(`../modules/${curName}/css/${currentMod.getCSS}`);
+  cssPile["local"].push(`../modules/${curName}/css/${currentMod.getCSS["local"]}`);
+  cssPile["external"].push(currentMod.getCSS["external"])
   //Adding script references
   currentMod.setScripts = currentMod.defineScripts();
   scriptPile["local"].push(`../modules/${curName}/js/${currentMod.getScripts["local"]}`);
-  console.log(scriptPile);
   scriptPile["external"].push(currentMod.getScripts["external"]);
   // Adding the current module to the dict under it's given name
   modDict[config.modules[i].name] = currentMod;

83341835928685c4c16cba04b9cbf0d71c493e74
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-06-25 11:46:27 -0400

    Added in express for sockets and backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+console.log("Loaded Express");
 // index setup
 require("./index.js");
 
-/*
 //Core setup
-require('./js/node/express-server.js');
+require('./src/core/express-server.js');
+/*
 require('./js/node/sys-info.js');
 //launchpad
 require("./js/node/commandExec.js");

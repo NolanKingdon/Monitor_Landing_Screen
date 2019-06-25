963b1d9f044a846c4384fe29034418fe9a2921ff
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-06-25 12:40:13 -0400

    Added in functions to add scripts to the DOM

--- src/core/moduleHandler.js
+++ src/core/moduleHandler.js
@@ -1,10 +1,16 @@
-console.log("Yay?")
 let config = require('../config/config.js');
 let modDict = {}; //Storing all of our modules by name
 let currentMod; //Reference variable for new modules
 let cssPile = [];
-let DOMPile = ``;
-let scriptPile = [];
+let DOMPile = { // TODO - add in dynamic columns to config file and instantiate here
+  0: ``,
+  1: ``,
+  2: ``
+};
+let scriptPile = {
+  local: [],
+  external: []
+};
 
 for(let i=0; i<config.modules.length; i++){
   //Loading the module into existance
@@ -12,14 +18,17 @@ for(let i=0; i<config.modules.length; i++){
   currentMod = require(`../modules/${curName}/${curName}.js`);
   currentMod = new currentMod(config.modules[i]);
   //Adding HTML to the template string
-  DOMPile += currentMod.getDOM;
+  console.log(config.modules[i].location);
+  DOMPile[config.modules[i].location] += currentMod.getDOM;
   //Adding CSS references
   // TODO - Think about making this specific using IDs or classes --> Take some work off of the user
   currentMod.setCSS = currentMod.defineCSS();
   cssPile.push(`../modules/${curName}/css/${currentMod.getCSS}`);
   //Adding script references
   currentMod.setScripts = currentMod.defineScripts();
-  scriptPile.push(currentMod.getScripts);
+  scriptPile["local"].push(`../modules/${curName}/js/${currentMod.getScripts["local"]}`);
+  console.log(scriptPile);
+  scriptPile["external"].push(currentMod.getScripts["external"]);
   // Adding the current module to the dict under it's given name
   modDict[config.modules[i].name] = currentMod;
   console.log(currentMod.getName + " load completed");
@@ -32,5 +41,5 @@ const pageTemplate = {
   scriptPile // List of lists
 }
 
-console.log(pageTemplate);
+// console.log(pageTemplate.DOMPile);
 module.exports = pageTemplate;

5853569f3267570ff03ca730727e35571d946330
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-12-11 22:38:06 -0500

    Rough refactor into an object

--- src/modules/Darksky/js/darksky.js
+++ src/modules/Darksky/js/darksky.js
@@ -7,91 +7,44 @@ class Tile {
             </div>`
         }
 }
-const skyconDict = {
-    "clear-day":"CLEAR_DAY",
-    "clear-night":"CLEAR_NIGHT",
-    "partly-cloudy-day":"PARTLY_CLOUDY_DAY",
-    "partly-cloudy-night":"PARTLY_CLOUDY_NIGHT",
-    "cloudy":"CLOUDY",
-    "rain":"RAIN",
-    "sleet":"SLEET",
-    "snow":"SNOW",
-    "wind":"WIND",
-    "fog":"FOG"
-}
-const day = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday"
-}
 
-let tileToggled = false;
+class DarkSky {
 
-function toggleTileContents(params, self){
-    if(tileToggled){
-        
-    } else {
-        // Our original
-        $(self).children("p")[0].innerHTML = `${params.tempMax}&deg;C / ${params.tempMin}&deg;C`;
-        
-        // Setting the day
-        $(self).children("p")[1].innerHTML = `${day[(params.today+count)%6]}`;
+    constructor(){
+        this.tiles = ``;
+        for(let i=0; i<6; i++){
+            this.tiles += new Tile().generateHTML(i);
+        }
+
+        this.skyconDict = {
+            "clear-day":"CLEAR_DAY",
+            "clear-night":"CLEAR_NIGHT",
+            "partly-cloudy-day":"PARTLY_CLOUDY_DAY",
+            "partly-cloudy-night":"PARTLY_CLOUDY_NIGHT",
+            "cloudy":"CLOUDY",
+            "rain":"RAIN",
+            "sleet":"SLEET",
+            "snow":"SNOW",
+            "wind":"WIND",
+            "fog":"FOG"
+        }
+        this.day = {
+            0: "Sunday",
+            1: "Monday",
+            2: "Tuesday",
+            3: "Wednesday",
+            4: "Thursday",
+            5: "Friday",
+            6: "Saturday"
+        }
+        this.createSkycons();
+        this.recentCall = this.getAPIData();
+
+        // Fade event -> Repurpose
+        $("#fader").click( () => this.fadeEvent());   
     }
-    // Inverting our toggle
-    tileToggled = !tileToggled;
-}
 
-$(document).ready(()=>{
-    // Fetching API info
-    // Generating tiles
-    const skycons = new Skycons({"color": "white"});
-    let tiles = ``;
-    for(let i=0; i<6; i++){
-        tiles += new Tile().generateHTML(i);
-    }
-    fetch("https://api.darksky.net/forecast/509a3e1b4a685c7dcfe210108d521b6e/42.2650,-79.8704?units=si&exclude=[minutely,flags]", {
-        headers: {}
-    })
-    .then(response => response.json())
-    .then(j => {
-        console.log(j);
-        let currentApparentTemp = Math.round(j.currently.apparentTemperature);
-        let currentTemp = Math.round(j.currently.temperature);
-        skycons.add("weather-main", Skycons[skyconDict[j.currently.icon]]);
-        $("#weather-main-actual").html(`${currentTemp}&deg;C`);
-        $("#weather-main-feels").html(`Feels like ${currentApparentTemp}&deg;C`);
-        $("#weather-main-description").html(j.currently.summary);
-        // Adding in tiles with appropriate skycon canvases
-        let today = new Date().getDay();
-        // Getting the appropriate division (Default to day)
-        count = 0;
-        $("#weather-right-side").html(tiles).children().each( function(){
-            let tempMax = Math.round(j.daily.data[count].apparentTemperatureMax)
-            let tempMin = Math.round(j.daily.data[count].apparentTemperatureMin)
-            let id = $(this).children("canvas")[0].id;
-            skycons.add(id, Skycons[skyconDict[j.daily.data[count].icon]]);
-            // // Setting High/lows
-            // $(this).children("p")[0].innerHTML = `${tempMax}&deg;C / ${tempMin}&deg;C`;
-            // // Setting the day
-            // $(this).children("p")[1].innerHTML = `${day[(today+count)%6]}`;
-
-            setInterval(toggleTileContents({
-                tempMax,
-                tempMin,
-                today
-            }, this), 10000);
-            count++;
-        });
-        skycons.play();
-        
-    });
-
-    // Fade event -> Repurpose
-    $("#fader").click( () => {
+    fadeEvent(){
         let children = $("#weather-right-side").children();
         let time = 75;
         let fadeTime = time;
@@ -108,5 +61,77 @@ $(document).ready(()=>{
 
         // Fading back in
         setTimeout(()=>{$("#weather-right-side").children().fadeIn(350);}, delayIn)
-    });
+    }
+
+    createSkycons(){
+        this.skycons = new Skycons({"color": "white"});
+    }
+
+    getAPIData(){
+        let json;
+        let skycons = this.skycons;
+        let skyconDict = this.skyconDict;
+        let self = this;
+        fetch("https://api.darksky.net/forecast/509a3e1b4a685c7dcfe210108d521b6e/42.2650,-79.8704?units=si&exclude=[minutely,flags]", {
+            headers: {}
+        })
+        .then(response => response.json())
+        .then(j => {
+            console.log(j);
+            let currentApparentTemp = Math.round(j.currently.apparentTemperature);
+            let currentTemp = Math.round(j.currently.temperature);
+            this.skycons.add("weather-main", Skycons[this.skyconDict[j.currently.icon]]);
+            $("#weather-main-actual").html(`${currentApparentTemp}&deg;C `);
+            $("#weather-main-feels").html(`(${currentTemp}&deg;C)`);
+            $("#weather-main-description").html(j.currently.summary);
+            // Adding in tiles with appropriate skycon canvases
+            let today = new Date().getDay();
+            // Getting the appropriate division (Default to day)
+            let count = 0;
+            $("#weather-right-side").html(this.tiles).children().each( function(){
+                console.log(skycons);
+                let tempMax = Math.round(j.daily.data[count].apparentTemperatureMax)
+                let tempMin = Math.round(j.daily.data[count].apparentTemperatureMin)
+                let id = $(this).children("canvas")[0].id;
+                skycons.add(id, Skycons[skyconDict[j.daily.data[count].icon]]);
+                // // Setting High/lows
+                // $(this).children("p")[0].innerHTML = `${tempMax}&deg;C / ${tempMin}&deg;C`;
+                // // Setting the day
+                // $(this).children("p")[1].innerHTML = `${day[(today+count)%6]}`;
+    
+                setInterval(self.toggleTileContents({
+                    tempMax,
+                    tempMin,
+                    today,
+                    count
+                }, this), 10000);
+                count++;
+            });
+            skycons.play();
+            json = j;
+        });
+        return json;
+    }
+
+    toggleTileContents(params, self){
+        let firstP = $(self).children("p")[0];
+        if(firstP.className != "temps"){
+            // Our original
+            firstP.innerHTML = `${params.tempMax}&deg;C to ${params.tempMin}&deg;C`;
+            firstP.className = "temps";
+            // Setting the day
+            $(self).children("p")[1].innerHTML = `${this.day[(params.today+params.count)%6]}`;
+        } else {
+
+        }
+        // Inverting our toggle
+
+    }
+}
+
+
+$(document).ready(()=>{
+    const darkSky = new DarkSky();
+    // Fetching API info
+    // Generating tiles
 });

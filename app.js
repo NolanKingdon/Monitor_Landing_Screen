b9fc728ed15ffbc2d4695d97381284649428c668
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-12-20 15:08:56 -0500

    added require for CMDExecuter backend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Core setup
 require('./src/core/express-server.js');
 require('./src/modules/Sys-info/backend/sys-info.js');
+require('./src/modules/CMD/backend/CMDExecuter.js');
 // index setup
 require("./index.js");
 
